75fea6b5e8d04ce13aa0937939029fb53a103ebf
feixiao <feixiao2020@sina.com>
2021-12-31 17:01:51 +0800

    cmake android 静态库

--- cmake/android/main.cpp
+++ cmake/android/main.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+#include "add.h"
+
 int main()
 {
     printf("Hello World!\n");
+    printf(" 1 + 1 = %d\n", add(1,1));
     return 0;
 }
---
+++ cmake/android/ndk_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export ANDROID_NDK=/Users/frank/android/ndk/android-ndk-r18b
+
+# For Ubuntu
+#export PATH=${PATH}:${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin
+
+# For OSX
+export PATH=${PATH}:${ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-x86_64/bin
+rm -r android_build
+mkdir android_build && cd android_build
+
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
+	-DANDROID_ABI="arm64-v8a" \
+	-DANDROID_NDK=$ANDROID_NDK \
+    -DCMAKE_SYSTEM_NAME=Android \
+	-DANDROID_PLATFORM=android-21 \
+	..
+
+make -j 4
