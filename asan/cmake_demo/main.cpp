1f457f1c2c3dfb11245a3e52e75b53879dd09704
feixiao <feixiao2020@sina.com>
2024-06-05 10:53:55 +0800

    M1 Prp not support detect_leaks

--- asan/cmake_demo/main.cpp
+++ asan/cmake_demo/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
+// 编译
+// cmake -DCMAKE_BUILD_TYPE=Debug ../  -DCMAKE_CXX_COMPILER=clang++
+// -DCMAKE_C_COMPILER=clang cmake -DCMAKE_BUILD_TYPE=Release ../
 
-// cmake -DCMAKE_BUILD_TYPE=Debug ../
-// cmake -DCMAKE_BUILD_TYPE=Release ../
+// 运行
+// ASAN_OPTIONS=detect_leaks=1 ./cmake_demo
 int main() {
-    auto p = malloc(7);
-    p = 0; // The memory is leaked here.
+  auto p = malloc(7);
+  p = 0;  // The memory is leaked here.
 
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
 }
